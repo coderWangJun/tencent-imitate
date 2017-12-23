a33ec3c4e73aa0c826685e7c75b224d25e1a026f
coderWangJun <1632869948@qq.com>
2017-12-24 00:23:21 +0800

    我最新提交的为你推荐模块到游戏模块 + 左右轮播图切换插件封装(JS+CSS)

--- js/tencent/lyc.js
+++ js/tencent/lyc.js
@@ -1,7 +1,143 @@
-/**
- * Created by yangbo on 2017/11/24.
- */
+~function ($) {
+    function PluginChangeBg() {
+        let $container = this;
 
-// 李毅超
+        ~function () {
+            $container.on("mouseover", function (e) {
+                let $target = $(e.target);
 
-let lyc = null;
+                $target.parents().each(function (index, item) {
+
+                    if ($(item).hasClass("imgLink")) {
+
+                        let $figure_keyframes_mask = $(item).find(".figure_keyframes_mask"),
+                            $figure_keyframes_pic = $(item).find(".figure_keyframes_pic"),
+                            $figure_keyframes_outer = $(item).find(".figure_keyframes_outer");
+
+                        //判断如果大图不存在，大图的alt值为空，mark层及进度条不再显示
+                        if($(item).find(".figure_keyframes_pic").attr("src")===""){
+                            $(item).find(".figure_keyframes_pic").attr("alt","").siblings().css("opacity", 0);
+                        }
+                        $figure_keyframes_pic.css("display", "block");//大图呈现
+                        $figure_keyframes_mask.css("display", "block");//mark显示
+                        $figure_keyframes_outer.css("display", "block");//进度条显示
+                    }
+                });
+            });
+        }();
+
+        ~function () {
+            $container.on("mousemove", function (e) {
+                let $target = $(e.target);
+                $target.parents().each(function (index, item) {
+
+                    if ($(item).hasClass("imgLink")) {
+                        let $figure_keyframes = $(item).find(".figure_keyframes"),
+                            $figure_keyframes_pic = $(item).find(".figure_keyframes_pic"),
+                            $figure_keyframes_inner = $(item).find(".figure_keyframes_inner"),
+                            $figure_keyframes_outer = $(item).find(".figure_keyframes_outer"),
+                            maxW = $figure_keyframes.outerWidth() - 8,//鼠标移动的最大值
+                            proW = $figure_keyframes_outer.outerWidth(),//底条宽度
+                            curW = $figure_keyframes.outerWidth();//大图父级盒子的宽度
+
+                        //当前鼠标位置距离$figure_keyframes的距离
+                        let strW = e.pageX - $figure_keyframes.offset().left - 4;
+
+                        //边界判断
+                        strW = strW < 4 ? strW : (strW > maxW ? maxW : strW);
+                        //白色进度条的进度
+                        $figure_keyframes_inner.css("width", strW / proW * 100 + "%");
+                        //根据大图中各个小图距离左边、上边的距离定义成一个二维数组。
+                        let L = $figure_keyframes.outerWidth(),
+                            T = $figure_keyframes.outerHeight();
+                        let ary = [[0, -L, -L * 2], [0, -T, -T * 2]];
+                        //let ary = [[0, -194, -388], [0, -137, -274]];
+
+                        //通过解构赋值，得到每一张图片在大图中的位置。
+                        let [[, a,], [b, ,]] = ary;
+                        let [[, , c], [d, ,]] = ary;
+                        let [[f, ,], [, g,]] = ary;
+                        let [[, h,], [, i,]] = ary;
+                        let [[, , j], [, k,]] = ary;
+                        let [[m, ,], [, , n]] = ary;
+                        let [[, x,], [, , y]] = ary;
+
+                        let meanValue = curW / 8;
+
+                        if (strW <= meanValue && strW > 0) {
+                            $figure_keyframes_pic.css({
+                                left: 0,
+                                top: 0
+                            });
+                        }
+                        if (strW <= meanValue * 2 && strW > meanValue) {
+                            $figure_keyframes_pic.css({
+                                left: a,
+                                top: b
+                            });
+                        }
+                        if (strW <= meanValue * 3 && strW > meanValue * 2) {
+                            $figure_keyframes_pic.css({
+                                left: c,
+                                top: d
+                            });
+                        }
+                        if (strW <= meanValue * 4 && strW > meanValue * 3) {
+                            $figure_keyframes_pic.css({
+                                left: f,
+                                top: g
+                            });
+                        }
+                        if (strW <= meanValue * 5 && strW > meanValue * 4) {
+                            $figure_keyframes_pic.css({
+                                left: h,
+                                top: i
+                            });
+                        }
+                        if (strW <= meanValue * 6 && strW > meanValue * 5) {
+                            $figure_keyframes_pic.css({
+                                left: j,
+                                top: k
+                            });
+                        }
+                        if (strW <= meanValue * 7 && strW > meanValue * 6) {
+                            $figure_keyframes_pic.css({
+                                left: m,
+                                top: n
+                            });
+                        }
+                        if (strW <= meanValue * 8 && strW > meanValue * 7) {
+                            $figure_keyframes_pic.css({
+                                left: x,
+                                top: y
+                            });
+                        }
+                    }
+                });
+            });
+        }();
+
+        ~function () {
+            $container.on("mouseout", function (e) {
+
+                let $target = $(e.target);
+
+                $target.parents().each(function (index, item) {
+                    if ($(item).hasClass("imgLink")) {
+                        let $figure_keyframes_mask = $(item).find(".figure_keyframes_mask"),
+                            $figure_keyframes_pic = $(item).find(".figure_keyframes_pic"),
+                            $figure_keyframes_outer = $(item).find(".figure_keyframes_outer");
+
+                        $figure_keyframes_pic.css("display", "none");//大图隐藏
+                        $figure_keyframes_mask.css("display", "none");//mark层隐藏
+                        $figure_keyframes_outer.css("display", "none");//进度条隐藏
+                    }
+                });
+            });
+        }();
+    }
+
+    $.fn.extend({
+        PluginChangeBg: PluginChangeBg
+    });
+}(jQuery);
--- js/tencent/wj.js
+++ js/tencent/wj.js
@@ -2,6 +2,435 @@
  * Created by yangbo on 2017/11/24.
  */
 
-// 王俊
+// 轮播图切换插件
+~function ($) {
+    let pluginTab = function (options) {
+        let $tabBox = this,//这个是大盒子，（必传参数，需要配置）
+            $tabList = $tabBox.find(".wj_tabList li"),
+            $prev = $tabBox.find("a.wj_prev"),
+            $next = $tabBox.find("a.wj_next");
 
-let wj = null;
+        let _default = {
+            len: 7,
+            arrowVal: -55,
+            total: 0
+        };
+        $.extend(this,_default,options);
+
+        this.attr({
+            len: _default.len,
+            arrowVal: _default.arrowVal,
+            total: _default.total,
+            liList: null,
+            start: null,
+            end: null,
+            drawing: null,
+            flag: 0
+        });
+
+        let _len = null,
+            _arrowVal = null,
+            _total = null,
+            _liList = null,
+            _start = null,
+            _end = null,
+            _drawing = null,
+            flag = 0;
+
+        $tabBox.on("click", function (e) {
+
+            _liList = $tabList.length;
+            _len = parseFloat($(this).attr("len"));
+            _arrowVal = parseFloat($(this).attr("arrowVal"));
+            _total = parseFloat($(this).attr("total"));
+            flag = parseFloat($(this).attr("flag"));
+
+            $(this).attr("drawing", _liList % _len);
+            _drawing = parseFloat($(this).attr("drawing"));
+
+            let $target = $(e.target),
+                elTagName = $target.prop("tagName");
+            if (elTagName === "A" && $target.hasClass("wj_next")) {
+                $prev.css({
+                    "left": _arrowVal,
+                    "display": "block"
+                });
+                $(this).attr("total", parseFloat(_total + _len));
+                _total = $(this).attr("total")-0;
+
+                // 判断li的总个数和传进来的len个数取模是否===0
+                if (!flag) {
+                    if (!(_liList % _len === 0)) {
+                        $(this).attr("data-start", _len - _drawing);
+                        console.log("2========="+_total);
+                        // 获取要创建几个li，从哪里开始循环创建的元素
+                        $(this).attr("end", $tabList.length - _drawing);
+                        _end = parseFloat($(this).attr("end"));
+
+                        $(this).attr("data-start", _end - parseFloat($(this).attr("data-start")));
+                        _start = parseFloat($(this).attr("data-start"));
+
+                        // 获取要创建的元素 [object HTMLLIElement]
+                        for (let i = _start; i < _end; i++) {
+                            // 添加元素到页面
+                            $tabList.eq(_end).before($($tabList[i]).clone(true));
+                        }
+
+                        // 重新获取元素和个数
+                        $tabList = $(this).find(".wj_tabList li");
+                        _liList = $tabList.length;
+                        _drawing = _liList % _len;
+
+                        $(this).attr("flag", 1);
+                        flag = parseFloat($(this).attr("flag"));
+
+                    }
+                }
+                //console.log("3========="+_total);
+                $tabList.each(function (index, item) {
+                    index < _total ? $(item).css("display", "none") : null;
+                });
+                if (_total >= $tabList.length - _len) {
+                    $target.css({
+                        "right": 0,
+                        "display": "none"
+                    });
+                    $(this).attr("total", $tabList.length - _len);
+                }
+                e.preventDefault();
+            }
+
+            if (elTagName === "A" && $target.hasClass("wj_prev")) {
+                $(this).attr("total", _total - _len);
+                _total = parseFloat($(this).attr("total"));
+                $next.css({
+                    "right": _arrowVal,
+                    "display": "block"
+                });
+                $tabList.each(function (index, item) {
+                    index >= _total ? $(item).css("display", "inline-block") : null;
+                });
+
+                if (_total <= 0) {
+                    $target.css({
+                        "left": 0,
+                        "display": "none"
+                    });
+                    $(this).attr("total", 0);
+                }
+                e.preventDefault();
+            }
+        });
+
+
+    };
+
+    $.fn.extend({
+        pluginTab: pluginTab
+    });
+
+}(jQuery);
+
+// 为你推荐
+let wjRecommendRender = (function ($) {
+    let $wjMoreRecommend = $("#wj_more_recommend"),
+        $wjMoreRecommendA = $wjMoreRecommend.find("a"),
+        str = ``,
+        len = 0,
+        $wjRecommenList = $("#wj_recommen_list"),
+        start = 0,
+        end = 0;
+
+    let ajax = function (num) {
+        if (num === 0) {
+            end = 10;
+        } else {
+            start = end + 1;
+            end = start + 6;
+        }
+        $.ajax({
+            url: "json/wj/recommend.json",
+            type: "get",
+            dataType: "json",
+            success: function (res) {
+                console.log(res.length, start, end);
+                for (let i = start; i <= end; i++) {
+                    let item = res[i];
+                    str += `<li>
+                <a href="javascript:;">
+                    <img src="${item.img}" alt="">
+                    <span class="mark_quickplay"></span>
+                    <div class="figure_count">${item.count}</div>
+                </a>
+                <a href="javascrip:;">
+                    <p>${item.title}</p>
+                </a>
+            </li>`;
+                }
+                $wjRecommenList.append(str);
+            }
+        });
+    };
+
+    let bindHtml = function () {
+        let timer = null;
+        $wjMoreRecommendA.on("click", function () {
+            let $this = $(this);
+            $this.text("正在加载......");
+            len++;
+            if (len === 1) {
+                timer = setTimeout(function () {
+                    ajax(len);
+                    $wjMoreRecommend.addClass("cur");
+                    $this.text("我是有底线的");
+                    clearTimeout(timer);
+                    return;
+                }, 800);
+
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            ajax(0);
+            bindHtml();
+        }
+    }
+})(jQuery);
+wjRecommendRender.init();
+
+// 娱乐热点
+let wjHotRender = (function ($) {
+    let $wj_HotList= $("#wj_HotList"),
+        str = ``;
+
+    let ajax = function () {
+        $.ajax({
+            url: "json/wj/carInfo.json",
+            type: "get",
+            dataType: "json",
+            success: function (res) {
+                $.each(res,function(index,item){
+                    str += `<li>
+                    <a class="imgLink figure" href="javascript:;">
+                        <img class="figure_pic" src="${item.img}" alt="">
+                        <span class="lyc_iconfont lyc_icon-shejishi-bofanganniu"></span>
+                        <div class="figure_keyframes">
+                            <img src="${item.bigImg}" alt="多张动态图" class="figure_keyframes_pic">
+                            <div class="figure_keyframes_mask"></div>
+                            <div class="figure_keyframes_outer">
+                                <div class="figure_keyframes_inner"></div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="figure_detail_org">
+                        <strong class="figure_title figure_title_f">
+                            <a class="tit_link" href="javascript:;" title="娱乐热点">娱乐热点
+                            </a>
+                        </strong>
+                        <div class="figure_desc" title="${item.title}">${item.title}
+                        </div>
+                    </div>
+                </li>`;
+                });
+                $wj_HotList.append(str);
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            ajax();
+        }
+    }
+})(jQuery);
+wjHotRender.init();
+
+// 音乐·演唱会
+let wjHotRender01 = (function ($) {
+    let $wj_HotList= $("#wj_HotList01"),
+        str = ``;
+
+    let ajax = function () {
+        $.ajax({
+            url: "json/wj/game.json",
+            type: "get",
+            dataType: "json",
+            success: function (res) {
+                $.each(res,function(index,item){
+                    str += `<li>
+                    <a class="imgLink figure" href="javascript:;">
+                        <img class="figure_pic" src="${item.img}" alt="">
+                        <span class="lyc_iconfont lyc_icon-shejishi-bofanganniu"></span>
+                        <div class="figure_keyframes">
+                            <img src="${item.bigImg}" alt="多张动态图" class="figure_keyframes_pic">
+                            <div class="figure_keyframes_mask"></div>
+                            <div class="figure_keyframes_outer">
+                                <div class="figure_keyframes_inner"></div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="figure_detail_org">
+                        <strong class="figure_title figure_title_f">
+                            <a class="tit_link" href="javascript:;" title="音乐·演唱会">音乐·演唱会
+                            </a>
+                        </strong>
+                        <div class="figure_desc" title="${item.title}">${item.title}
+                        </div>
+                    </div>
+                </li>`;
+                });
+                $wj_HotList.append(str);
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            ajax();
+        }
+    }
+})(jQuery);
+wjHotRender01.init();
+
+// NBA
+let wjHotRender02 = (function ($) {
+    let $wj_HotList= $("#wj_HotList02"),
+        str = ``;
+
+    let ajax = function () {
+        $.ajax({
+            url: "json/wj/game.json",
+            type: "get",
+            dataType: "json",
+            success: function (res) {
+                $.each(res,function(index,item){
+                    str += `<li>
+                    <a class="imgLink figure" href="javascript:;">
+                        <img class="figure_pic" src="${item.img}" alt="">
+                        <span class="lyc_iconfont lyc_icon-shejishi-bofanganniu"></span>
+                        <div class="figure_keyframes">
+                            <img src="${item.bigImg}" alt="多张动态图" class="figure_keyframes_pic">
+                            <div class="figure_keyframes_mask"></div>
+                            <div class="figure_keyframes_outer">
+                                <div class="figure_keyframes_inner"></div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="figure_detail_org">
+                        <strong class="figure_title figure_title_f">
+                            <a class="tit_link" href="javascript:;" title="NBA">NBA
+                            </a>
+                        </strong>
+                        <div class="figure_desc" title="${item.title}">${item.title}
+                        </div>
+                    </div>
+                </li>`;
+                });
+                $wj_HotList.append(str);
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            ajax();
+        }
+    }
+})(jQuery);
+wjHotRender02.init();
+
+// 最强笑点
+let wjHotRender03 = (function ($) {
+    let $wj_HotList= $("#wj_HotList03"),
+        str = ``;
+
+    let ajax = function () {
+        $.ajax({
+            url: "json/wj/entertainment.json",
+            type: "get",
+            dataType: "json",
+            success: function (res) {
+                $.each(res,function(index,item){
+                    str += `<li>
+                    <a class="imgLink figure" href="javascript:;">
+                        <img class="figure_pic" src="${item.img}" alt="">
+                        <span class="lyc_iconfont lyc_icon-shejishi-bofanganniu"></span>
+                        <div class="figure_keyframes">
+                            <img src="${item.bigImg}" alt="多张动态图" class="figure_keyframes_pic">
+                            <div class="figure_keyframes_mask"></div>
+                            <div class="figure_keyframes_outer">
+                                <div class="figure_keyframes_inner"></div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="figure_detail_org">
+                        <strong class="figure_title figure_title_f">
+                            <a class="tit_link" href="javascript:;" title="最强笑点">最强笑点
+                            </a>
+                        </strong>
+                        <div class="figure_desc" title="${item.title}">${item.title}
+                        </div>
+                    </div>
+                </li>`;
+                });
+                $wj_HotList.append(str);
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            ajax();
+        }
+    }
+})(jQuery);
+wjHotRender03.init();
+
+// 游戏
+let wjHotRender04 = (function ($) {
+    let $wj_HotList= $("#wj_HotList04"),
+        str = ``;
+
+    let ajax = function () {
+        $.ajax({
+            url: "json/wj/bighaha.json",
+            type: "get",
+            dataType: "json",
+            success: function (res) {
+                $.each(res,function(index,item){
+                    str += `<li>
+                    <a class="imgLink figure" href="javascript:;">
+                        <img class="figure_pic" src="${item.img}" alt="">
+                        <span class="lyc_iconfont lyc_icon-shejishi-bofanganniu"></span>
+                        <div class="figure_keyframes">
+                            <img src="${item.bigImg}" alt="多张动态图" class="figure_keyframes_pic">
+                            <div class="figure_keyframes_mask"></div>
+                            <div class="figure_keyframes_outer">
+                                <div class="figure_keyframes_inner"></div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="figure_detail_org">
+                        <strong class="figure_title figure_title_f">
+                            <a class="tit_link" href="javascript:;" title="游戏">游戏
+                            </a>
+                        </strong>
+                        <div class="figure_desc" title="${item.title}">${item.title}
+                        </div>
+                    </div>
+                </li>`;
+                });
+                $wj_HotList.append(str);
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            ajax();
+        }
+    }
+})(jQuery);
+wjHotRender04.init();
